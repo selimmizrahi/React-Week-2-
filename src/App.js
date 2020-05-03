dbb92dca3fce5e8f4860c80c2535e2acc20cd695
selimmizrahi <mizrahiselim@gmail.com>
2020-05-03 19:15:48 +0300

    React Week 2 Day 1 Exercises

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Api from "./components/api"
+// import Form from './day2/Ex4.Geekout/form'
+
+function App() {
+  return (
+    <Api></Api>
+    );
+}
+export default App;
